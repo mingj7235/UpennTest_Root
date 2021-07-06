dfa5849791791e5a06a89b5461819f51f2f2cab8
김민재 <joshuara7235@gmail.com>
2021-07-06 22:44:55 +0900

    Presentation Tier -> connection to boards-save.mustache

--- UpennTest-web/src/main/java/com/joshua/web/IndexController.java
+++ UpennTest-web/src/main/java/com/joshua/web/IndexController.java
@@ -6,8 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -20,5 +18,11 @@ public class IndexController {
         model.addAttribute("boards", boardsService.findAllDesc());
 
         return "index";
+    }
+
+    @GetMapping("/boards/save")
+    public String save (Model model) {
+
+        return "boards-save";
     }
 }
