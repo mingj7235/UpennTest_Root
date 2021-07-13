7a4ebe8e07c58015ea34ff5a5acedecf58d97c3f
mingJ7235 <joshuara7235@gmail.com>
2021-07-13 10:13:11 +0900

    Join Presentation Tier made

--- UpennTest-web/src/main/java/com/joshua/web/IndexController.java
+++ UpennTest-web/src/main/java/com/joshua/web/IndexController.java
@@ -1,26 +1,34 @@
 package com.joshua.web;
 
+import com.joshua.domain.members.Member;
 import com.joshua.dto.boards.BoardsResponseDto;
 import com.joshua.service.boards.BoardsService;
+import com.joshua.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final BoardsService boardsService;
+    private final MemberService memberService;
 
 
-    @GetMapping("/")
-    public String index (Model model) {
+    @RequestMapping (value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+    public String index (Member member, HttpServletRequest req, RedirectAttributes rttr, Model model) throws Exception {
+
+        HttpSession session = req.getSession();
 
         model.addAttribute("boards", boardsService.findAllDesc());
-        //test
-
-
         return "index";
     }
 
@@ -50,11 +58,16 @@ public class IndexController {
         return "boards/boards-update";
     }
 
+    @GetMapping ("/member/join")
+    public String join () {
+        return "member/join";
+    }
+
+
+
     @GetMapping ("/member/mypage/{id}")
     public String mypage (@PathVariable Long id, Model model) {
         System.out.println("들어옴");
-
-
 
         return "member/member-mypage";
     }
